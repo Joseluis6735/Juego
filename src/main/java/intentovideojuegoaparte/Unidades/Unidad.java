cb24e5a192dbb0bd24419951d7b57b73e1349046
Joseluis6735 <josel.cano@edu.uah.es>
2025-05-26 15:46:42 +0200

    terminado

--- src/main/java/intentovideojuegoaparte/Unidades/Unidad.java
+++ src/main/java/intentovideojuegoaparte/Unidades/Unidad.java
@@ -1,5 +1,7 @@
 package intentovideojuegoaparte.Unidades;
 
+import java.util.Random;
+
 public abstract class Unidad {
     protected String nombre;
     public int hp;
@@ -9,6 +11,7 @@ public abstract class Unidad {
     protected int rangoAtaque;
     protected int x, y;
     protected String faccion;
+    private static final Random random = new Random();
 
     public Unidad(String nombre, int hp, int ataque, int defensa, int rangoMovimiento, int rangoAtaque, String faccion) {
         this.nombre = nombre;
@@ -41,8 +44,34 @@ public abstract class Unidad {
     }
 
     public void atacar(Unidad objetivo) {
-        int factor = (int) (Math.random() * 3); // 0, 1, 2
-        int danio = Math.max(0, (factor * ataque) - objetivo.defensa);
-        objetivo.recibirDanio(danio);
+        int dado = random.nextInt(6) + 1; // 1 a 6
+        int danioBase = Math.max(0, ataque - objetivo.defensa);
+        int danioFinal;
+
+        if (esCientifico()) {
+            // Ciencias: si dado es 1-3 duplica daño, sino daño base
+            if (dado >= 1 && dado <= 3) {
+                danioFinal = danioBase * 2;
+            } else {
+                danioFinal = danioBase;
+            }
+        } else {
+            // Letras: si dado es 4-6 duplica daño, sino daño base
+            if (dado >= 4 && dado <= 6) {
+                danioFinal = danioBase * 2;
+            } else {
+                danioFinal = danioBase;
+            }
+        }
+
+        objetivo.recibirDanio(danioFinal);
+    }
+
+    private boolean esCientifico() {
+        // Consideramos que Ciencias son estas facciones:
+        // "Científicos" o las que definas para esa facción
+        // O también puedes decidirlo por nombre de unidad
+        // Mejor usar la facción:
+        return faccion.equalsIgnoreCase("Científicos");
     }
 }
